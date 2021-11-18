1af88eccb3e7f18f4dddf5b45ff98fd0229b64f3
Mahed <mahedyar@gmail.com>
2021-11-18 10:26:42 +0330

    secondChanges

--- todoList.js
+++ todoList.js
@@ -1,5 +1,8 @@
 const list = document.getElementById("list");
 const input = document.getElementById("input")
+const CHECK = "fa-check-circle"
+const UNCHECK = "fa-circle-thin"
+const LINE_THROUGH = "lineThrough"
 let LIST = [] ;
 let id = 0 ;
 
@@ -19,18 +22,31 @@ document.addEventListener("keyup",function (event){
 
         }
         input.value =""
-        
+        id++ ;
     }
 });
 
-function addtoDo (toDo){
+function completeToDo(element){
+    element.classList.toggle(CHECK)
+    element.classList.toggle(UNCHECK)
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH)
+    // LIST[element.id].done = LIST[element.id].done ? false : true ;
+    LIST[element.id].done = !LIST[element.id].done  ;
+}
+
+function addtoDo (toDo,id,done){
+
+    if (trash){return;}
+
+    const DONE = done ? CHECK : UNCHECK
+    const LINE = done ? LINE_THROUGH : ""
 
     const text =
         `
     <li class="item">
-                <i class="fa fa-circle-thin complete" job="complete"></i>
-                <p class="text">${toDo}</p>
-                <i class="fa fa-trash-o delete" job="delete"></i>
+                <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
+                <p class="text ${LINE}" >${toDo}</p>
+                <i class="fa fa-trash-o delete" job="delete" id="${id}"></i>
     </li>
     
     
